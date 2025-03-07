5b8ac3a51c3c5aa84030f06b2503ea14c8be8e89
ru80john <s112032532@m112.nthu.edu.tw>
2025-03-07 15:06:19 +0800

    modify tinker poledit input

---
+++ include/LICHEM_matrix.h
@@ -0,0 +1,44 @@
+/*
+
+###############################################################################
+#                                                                             #
+#                 LICHEM: Layered Interacting CHEmical Models                 #
+#                 By: Eric G. Kratz, Hatice Gokcan, Alice Walker,             #
+#                     Erik Montelongo Vazquez, G. Andres Cisneros             #
+#                                                                             #
+#                      Symbiotic Computational Chemistry                      #
+#                                                                             #
+###############################################################################
+
+ Matrix headers and libraries used in LICHEM.
+
+*/
+
+//Make including safe
+#ifndef LICHEM_MATRIX
+#define LICHEM_MATRIX
+
+//Header files
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/LU>
+#include <Eigen/QR>
+#include <Eigen/SVD>
+#include <Eigen/Eigenvalues>
+#include <Eigen/StdList>
+#include <Eigen/Eigen>
+#include <Eigen/StdVector>
+
+//#include "../Eigen3/Eigen/Core"
+//#include "../Eigen3/Eigen/Dense"
+//#include "../Eigen3/Eigen/LU"
+//#include "../Eigen3/Eigen/QR"
+//#include "../Eigen3/Eigen/SVD"
+//#include "../Eigen3/Eigen/Eigenvalues"
+//#include "../Eigen3/Eigen/StdList"
+//#include "../Eigen3/Eigen/Eigen"
+//#include "../Eigen3/Eigen/StdVector"
+
+
+#endif
+
